6b9dd596550a6b06e05ad8e86ee43f8f39fbfb7b
JulianJAM <Jalbarracinmayorquin4@gmail.com>
2026-02-16 12:46:51 -0500

    Add files via upload

---
+++ Corte 1/Practica 1/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+/**
+ *
+ * @author Julian: 02/02/
+ */
+public class HelloWorld {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    
+    }
+    
+}
